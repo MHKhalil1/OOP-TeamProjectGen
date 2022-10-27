9c5053969f87dfd8338f9fb86ad8d39b0a62011e
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-10-26 20:50:55 -0400

    Intern Test Complete

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("Create school.", () => {
+    const testSchool = "School Name";
+    const employeeSample = new Intern("Mahmoud", 1, "khalilmahmoud2333@gmail.com", testSchool);
+    expect(employeeSample.school).toBe(testSchool);
+});
+
+test("Test if officeNumber will return office number.", () => {
+    const testSchool = "School Name";
+    const employeeSample = new Intern("Mahmoud", 1, "khalilmahmoud2333@gmail.com", testSchool);
+    expect(employeeSample.getSchool()).toBe(testSchool);
+});
+
+test("Test role.", () => {
+    const returnValue = "Intern";
+    const employeeSample = new Intern("Mahmoud", 1, "khalilmahmoud2333@gmail.com", "School Name");
+    expect(employeeSample.getRole()).toBe(returnValue);
+});
